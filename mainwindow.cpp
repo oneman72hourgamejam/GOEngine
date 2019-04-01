0aa70887d280aa0b4fd1e9708912df1404e04603
oneman72hourgamejam <oneman72hourgamejam@gmail.com>
2019-04-02 01:22:49 +0300

    debug for camera settings

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include<QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -16,14 +17,17 @@ MainWindow::~MainWindow()
 void MainWindow::on_camAngle_valueChanged(double arg1)
 {
     this->ui->widget->camAngle = arg1;
+    qDebug() << "camAngle" << arg1;
 }
 
 void MainWindow::on_camNearPlane_valueChanged(double arg1)
 {
     this->ui->widget->camNearPlane = arg1;
+    qDebug() << "camNearPlane" << arg1;
 }
 
 void MainWindow::on_camFarPlane_valueChanged(double arg1)
 {
     this->ui->widget->camFarPlane = arg1;
+    qDebug() << "camFarPlane" << arg1;
 }
